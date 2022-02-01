6ad52e2156e8ca6d5fbda95d6c62f3991401ba19
ramiroPezzone <ramiro.pezzone@gmail.com>
2022-02-01 18:24:52 -0300

    section techs mejorada

--- skills/back.js
+++ skills/back.js
@@ -1,14 +1,14 @@
 const back = [
     {
+        tecnologia: 'JS',
+        logo: 'js-logo.png',
+        descripcion: 'Conocimientos intermedios/avanzados en lenguaje de programación JS'
+    },
+    {
         tecnologia: 'NodeJS',
         logo: 'node-js-logo.png',
         descripcion: 'Conocimientos intermedios en NodeJS'
     },
-    {
-        tecnologia: 'JS',
-        logo: 'js-logo.png',
-        descripcion: 'Conocimientos intermedios/avanzados en lenguaje de programación JS'
-    }
 
 ]
 
