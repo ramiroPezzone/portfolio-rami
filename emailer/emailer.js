b9e6a8a1629804b610fb9525f22ed8268cc3a9fa
ramiroPezzone <ramiro.pezzone@gmail.com>
2022-04-20 08:33:34 -0300

    Eliminación de líneas innecesarias de configuración de fecha del archivo Mailer

--- emailer/emailer.js
+++ emailer/emailer.js
@@ -4,62 +4,7 @@ let express = require("express"),
   usuario = "ramiro.pezzone.emailer@gmail.com",
   contrasenia = "aiclfwwgpgovmrak",
   enviarA = "ramiro.pezzone@gmail.com",
-  //   CONFIGURACIÓN DE FECHA Y HORA
-  dateNow = new Date().toLocaleString()
-//   mes = dateNow.getMonth(),
-//   dia = dateNow.getDate(),
-//   diaDeLaSemana = dateNow.getDay(),
-//   hora = dateNow.getHours(),
-//   minutos = dateNow.getMinutes();
-
-// SETEANDO LOS MINUTOS
-// if (minutos < 10) {
-//   minutos = "0" + minutos;
-// }
-
-// SETEANDO EL DÍA DE LA SEMANA
-// if (diaDeLaSemana == 0) {
-//   diaDeLaSemana = "Domingo";
-// } else if (diaDeLaSemana == 1) {
-//   diaDeLaSemana = "Lunes";
-// } else if (diaDeLaSemana == 2) {
-//   diaDeLaSemana = "Martes";
-// } else if (diaDeLaSemana == 3) {
-//   diaDeLaSemana = "Miércoles";
-// } else if (diaDeLaSemana == 4) {
-//   diaDeLaSemana = "Jueves";
-// } else if (diaDeLaSemana == 5) {
-//   diaDeLaSemana = "Viernes";
-// } else {
-//   diaDeLaSemana = "Sábado";
-// }
-
-// SETEANDO EL MES
-// if (mes === 0) {
-//   mes = "Enero";
-// } else if (mes == 1) {
-//   mes = "Febrero";
-// } else if (mes == 2) {
-//   mes = "Marzo";
-// } else if (mes == 3) {
-//   mes = "Abril";
-// } else if (mes == 4) {
-//   mes = "Mayo";
-// } else if (mes == 5) {
-//   mes = "Junio";
-// } else if (mes == 6) {
-//   mes = "Julio";
-// } else if (mes == 7) {
-//   mes = "Agosto";
-// } else if (mes == 8) {
-//   mes = "Septiembre";
-// } else if (mes == 9) {
-//   mes = "Octubre";
-// } else if (mes == 10) {
-//   mes = "Noviembre";
-// } else if (mes == 11) {
-//   mes = "Diciembre";
-// }
+  dateNow = new Date().toLocaleString();
 
 router.post("/contacto", async (req, res) => {
   const { nombre, tel, email, company, mensaje } = req.body;
